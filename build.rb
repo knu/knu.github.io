09b7290e7ad378a206dca10b30eee55192e2c3fb
Akinori MUSHA <knu@idaemons.org>
2023-07-03 01:49:39 +0900

    Edit the frontmatter section of each file under pages/

---
+++ build.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+require "yaml"
+require "uri"
+
+Dir["pages/*.md"].each do |md|
+  content = File.read(md)
+  basename = File.basename(md, ".md")
+
+  content.sub!(/\A---\n(?:.*\n)*(?=---\n)/) { |frontmatter|
+    yaml = YAML.load(frontmatter)
+    title = yaml["title"] ||= basename
+    yaml["permalink"] ||= "/#{URI.encode_uri_component(title)}"
+    YAML.dump(yaml)
+  }
+
+  File.write(md, content)
+end
